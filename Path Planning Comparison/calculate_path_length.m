9757464f9785c39e3576fa18ac93604eabe66d8f
abhimarathe98 <abhishek.marathe@tu-dortmund.de>
2024-11-16 18:14:56 +0100

    Delete Path Planning Comparison directory

--- Path Planning Comparison/calculate_path_length.m
+++
@@ -1,9 +0,0 @@
-function length = calculate_path_length(path)
-    if isempty(path)
-        length = inf;
-        return;
-    end
-    diffs = diff(path);
-    segment_lengths = sqrt(sum(diffs.^2, 2));
-    length = sum(segment_lengths);
-end
